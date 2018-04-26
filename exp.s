e0a9d60f6464c8fcd74548f4405feca8129befe0
MWessel27 <mwessel27@gmail.com>
2018-04-25 22:50:02 -0400

    comments & cleanup

--- exp.s
+++ exp.s
@@ -1,14 +1,15 @@
 # Mikalangelo David Wessel - 04/25/2018
 # mdw15d
-# exp.s - a simple driver program that tests the double factorial
-#	function written by Professor Leach
+# exp.s - a driver program and calculations for powers of e
+#
 # Register use:
-#	$a0	parameters for factorial start number and syscall
+#	$f0	parameters for factorial start number and syscall
 #	$v0	syscall parameter
 #	$t0 temporary calculations
 #   $f0 floating point answer
-#   $f2 floating point calculations
-#	$f12 floating point return
+#   $f2-10,$f14,$f18 floating point calculations
+#	$f12 floating point parameter
+#   $f16 exit flag
 
 exp:
       l.d   $f2, cons0       # $f2 = 0.0
@@ -19,74 +20,66 @@ exp:
       mov.d $f10, $f12       # second copy of term 
       abs.d $f8, $f8         # get absolute value
 
-      l.d   $f20, cons1      # set n to 1
-      div.d $f14, $f8, $f20   # divide x by n
-      add.d $f18, $f4, $f14   # add term 0(1) to x/n
+      l.d   $f20, cons1      # n=1
+      div.d $f14, $f8, $f20  # x/n
+      add.d $f18, $f4, $f14  # add
 
 again:
-      add.d $f20, $f20, $f4  # add 1 to n
-      mul.d $f14, $f14, $f8  # multiply old term by x
-      div.d $f14, $f14, $f20 # divide by incremented n
+      add.d $f20, $f20, $f4  # 1+n
+      mul.d $f14, $f14, $f8  # multiply previous term by x
+      div.d $f14, $f14, $f20 # divide by 1+n
 
-      c.lt.d $f14, $f6       # compare term to 1.0e-15
+      c.lt.d $f14, $f6       # less than 1.0e-15
       bc1t   done
 
       add.d $f18, $f18, $f14 # add result into total
 
-	  j	again		# and loop
+	  j	again		         # and loop
 
 done:
       mov.d     $f0, $f18    # move into return
- 	  jr	    $ra		# return to calling routine
+ 	  jr	    $ra		     # return to calling routine
 
-main: la	$a0, intro	# print intro
+main: la	$a0, intro	     # print intro
       li	$v0, 4
 	  syscall
 
-loop: la	$a0, req	# request value of x
+loop: la	$a0, req	     # request value of x
 	  li	$v0, 4
 	  syscall
 	  
-	  li	$v0, 7		# read value of x
+	  li	$v0, 7		     # read value of x
 	  syscall
 
-      l.d     $f16, flag  # exit if 999 was entered
+      l.d     $f16, flag     # exit if 999 was entered
       c.eq.d  $f16, $f0
       bc1t    out
 	  
-	  la	$a0, ans	# print text part of answer
+	  la	$a0, ans	     # print text part of answer
 	  li	$v0, 4
 	  syscall
 
       li	$v0, 3
-      mov.d $f12, $f0   # display value of x
+      mov.d $f12, $f0        # display value of x
       syscall
 
-      la	$a0, cr	    # print text part of answer
-	  li	$v0, 4
-	  syscall
-
-      #move	$a0, $s2	# place value of n in $a0
-	  
-	  jal	exp		    # call exp function
-	  
-	  #move  $s1, $v0	# save value returned by fact
-
-	  #li	$v0, 3      # prepare to read out double prec value
-	  #mov.d $f12, $f0   # move into FP return register
-	  #syscall
-
-      li	$v0, 3      # prepare to read out double prec value
-	  mov.d $f12, $f0   # move into FP return register
-	  syscall
-	  
-	  j	loop	        # branch back and next value of n
-
-out:  la	$a0, adios	# display closing
+      la	$a0, cr	         # print text part of answer
 	  li	$v0, 4
 	  syscall
 	  
-	  li	$v0, 10		# exit from the program
+	  jal	exp		         # call exp function
+
+      li	$v0, 3           # prepare to read out double prec value
+	  mov.d $f12, $f0        # move into FP return register
+	  syscall
+	  
+	  j	loop	             # branch back and next value of n
+
+out:  la	$a0, adios	     # display closing
+	  li	$v0, 4
+	  syscall
+	  
+	  li	$v0, 10		     # exit from the program
 	  syscall
 	
 	  .data
